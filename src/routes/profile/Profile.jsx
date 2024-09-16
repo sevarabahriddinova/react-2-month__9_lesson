5a6d57b7ed07485472235afcbad5317867fc4db5
sevarabahriddinova <sevaraxonbahriddinova@gmail.com>
2024-09-16 18:28:02 +0500

    design

--- src/routes/profile/Profile.jsx
+++ src/routes/profile/Profile.jsx
@@ -1,3 +1,5 @@
+import { BsStars } from "react-icons/bs"; 
+import { RxAvatar } from "react-icons/rx"; 
 import React from 'react'
 import { useGetProfileQuery } from '../../redux/api/profile'
 
@@ -8,13 +10,27 @@ const Profile = () => {
   console.log(data)
 
   return (
-    <div>
+    <div className='w-[600px] m-auto mt-12 shadow-xl p-4 '>
+      <div>
       {
         data ? (<div>
-          <img src={data.payload.photo_url} alt="" />
-          <p>{data.payload.first_name}</p>
-          <p>{data.payload.username}</p>
+          
+          <div className='flex flex-col items-center gap-8 p-5'>
+            <p className='font-bold text-[28px] uppercase  text-blue-800'>{data.payload.first_name}</p>
+            <img className='w-[400px] rounded-md' src={data.payload.photo_url} alt="" />
+          <div className="flex items-center gap-4">
+          <BsStars /><BsStars /><BsStars /><BsStars /><BsStars />
+          <p className="text-[20px]">{data.payload.username}</p>
+          </div>
+         <div className="flex items-center gap-4">
+          <BsStars /><BsStars /><BsStars /><BsStars /><BsStars />
+          <div className="flex gap-2 items-center">
+          <RxAvatar className="text-[24px] shadow-xl  " />
           <p>{data.payload.role}</p>
+          </div>
+         </div>
+          
+          </div>
 
           </div>
           ) : (
@@ -22,6 +38,7 @@ const Profile = () => {
           )
       }
       
+    </div>
     </div>
   )
 }
--- src/routes/singlPage/SinglPage.jsx
+++ src/routes/singlPage/SinglPage.jsx
@@ -11,15 +11,23 @@ const SinglPage = () => {
     const product=data?.payload
 
   return (
-    <div>
+    <div className='w-[600px] mt-16 rounded-2xl shadow-2xl p-6 m-auto '>
+        <div>
         {
             product? (<div>
-                <p>{product.product_name}</p>
-                <strong>${product.sale_price}</strong>
-                <img src={product.product_images[1]} alt="" />
+                <p className='font-bold text-[28px] text-blue-600 text-center'>{product.product_name}</p>
+                <div className='flex items-center mt-8'>
+                <img className='max-w-[300px]' src={product.product_images[1]} alt="" />
+                <div className='flex flex-col gap-4'>
                 <p>{product.description}</p>
+                <strong className=''>${product.sale_price}</strong>
+                </div>
+                </div>
+                
+                
             </div>) : (<p>loading</p>)
         }
+    </div>
     </div>
   )
 }
