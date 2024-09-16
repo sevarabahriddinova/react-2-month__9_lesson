154a789cde2724af15ea48d88a4c7bd75a1529e5
sevarabahriddinova <sevaraxonbahriddinova@gmail.com>
2024-09-16 18:33:41 +0500

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ const Header = () => {
     <div className=" bg-blue-500 shadow-2xl">
         <nav className='w-[1200px] m-auto flex items-center justify-between p-2'>
 
+        <Link to={"/"} >
         <div className='flex px-2 shadow-2xl bg-gray-300 w-fit rounded-full'>
             <span className='font-bold text-[36px]  text-lime-700'>S</span>
             <div className="flex flex-col text-lg">
@@ -14,7 +15,7 @@ const Header = () => {
                 <span><TbBrandGravatar className="text-yellow-600  absolute" /></span>
             </div >
             <span className='font-bold  text-[22px]  left-[44px] mt-2 text-green-700'>M</span>
-        </div>
+        </div></Link>
     
 
             <ul className='flex gap-6'>
